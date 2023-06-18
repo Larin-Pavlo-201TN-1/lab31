863db228f5f17e28745a5292f12142d665f2e012
Larin-Pavlo-201TN-1 <98223565+Larin-Pavlo-201TN-1@users.noreply.github.com>
2023-06-18 19:12:07 +0300

    Add files via upload

---
+++ lab31/lab31/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace lab31
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.processList = new System.Windows.Forms.ListView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.InformToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.StopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rebtn = new System.Windows.Forms.Button();
+            this.Savebtn = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // processList
+            // 
+            this.processList.ContextMenuStrip = this.contextMenuStrip1;
+            this.processList.HideSelection = false;
+            this.processList.Location = new System.Drawing.Point(150, 12);
+            this.processList.Name = "processList";
+            this.processList.Size = new System.Drawing.Size(638, 426);
+            this.processList.TabIndex = 0;
+            this.processList.UseCompatibleStateImageBehavior = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.InformToolStripMenuItem,
+            this.StopToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(246, 52);
+            // 
+            // InformToolStripMenuItem
+            // 
+            this.InformToolStripMenuItem.Name = "InformToolStripMenuItem";
+            this.InformToolStripMenuItem.Size = new System.Drawing.Size(245, 24);
+            this.InformToolStripMenuItem.Text = "Інформація про процес";
+            this.InformToolStripMenuItem.Click += new System.EventHandler(this.InformToolStripMenuItem_Click);
+            // 
+            // StopToolStripMenuItem
+            // 
+            this.StopToolStripMenuItem.Name = "StopToolStripMenuItem";
+            this.StopToolStripMenuItem.Size = new System.Drawing.Size(245, 24);
+            this.StopToolStripMenuItem.Text = "Зупинка процесу";
+            this.StopToolStripMenuItem.Click += new System.EventHandler(this.StopToolStripMenuItem_Click);
+            // 
+            // rebtn
+            // 
+            this.rebtn.Location = new System.Drawing.Point(12, 125);
+            this.rebtn.Name = "rebtn";
+            this.rebtn.Size = new System.Drawing.Size(132, 37);
+            this.rebtn.TabIndex = 1;
+            this.rebtn.Text = "Оновити";
+            this.rebtn.UseVisualStyleBackColor = true;
+            this.rebtn.Click += new System.EventHandler(this.rebtn_Click);
+            // 
+            // Savebtn
+            // 
+            this.Savebtn.Location = new System.Drawing.Point(12, 260);
+            this.Savebtn.Name = "Savebtn";
+            this.Savebtn.Size = new System.Drawing.Size(132, 45);
+            this.Savebtn.TabIndex = 2;
+            this.Savebtn.Text = "Зберегти список";
+            this.Savebtn.UseVisualStyleBackColor = true;
+            this.Savebtn.Click += new System.EventHandler(this.Savebtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Savebtn);
+            this.Controls.Add(this.rebtn);
+            this.Controls.Add(this.processList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView processList;
+        private System.Windows.Forms.Button rebtn;
+        private System.Windows.Forms.Button Savebtn;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem InformToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem StopToolStripMenuItem;
+    }
+}
+
---
+++ lab31/lab31/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab31
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            processList.Items.Clear();
+            Process[] processes = Process.GetProcesses();
+            foreach (Process process in processes)
+            {
+                ListViewItem item = new ListViewItem(process.ProcessName);
+                item.SubItems.Add(process.Id.ToString());
+                processList.Items.Add(item);
+            }
+
+        }
+
+        private void rebtn_Click(object sender, EventArgs e)
+        {
+            processList.Items.Clear();
+            Process[] processes = Process.GetProcesses();
+            foreach (Process process in processes)
+            {
+                ListViewItem item = new ListViewItem(process.ProcessName);
+                item.SubItems.Add(process.Id.ToString());
+                processList.Items.Add(item);
+            }
+        }
+
+        private void Savebtn_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Text Files|*.txt";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = saveFileDialog.FileName;
+                    using (StreamWriter writer = new StreamWriter(filePath))
+                    {
+                        foreach (ListViewItem item in processList.Items)
+                        {
+                            writer.WriteLine($"Process Name: {item.Text}\tID: {item.SubItems[1].Text}");
+                        }
+                    }
+                    MessageBox.Show("Process list exported successfully.",
+                                    "Export Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void InformToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (processList.SelectedItems.Count > 0)
+            {
+                string processName = processList.SelectedItems[0].Text;
+                Process process = Process.GetProcessesByName(processName)[0];
+
+                string processInfo = $"Process Name: {process.ProcessName}\n" +
+                                     $"ID: {process.Id}\n" +
+                                     $"Start Time: {process.StartTime}\n" +
+                                     $"Responding: {process.Responding}\n" +
+                                     $"Threads: {process.Threads.Count}\n" +
+                                     $"Modules: {process.Modules.Count}\n\n";
+
+                processInfo += "Threads:\n";
+                foreach (ProcessThread thread in process.Threads)
+                {
+                    processInfo += $"Thread ID: {thread.Id}\tState: {thread.ThreadState}\n";
+                }
+
+                processInfo += "\nModules:\n";
+                foreach (ProcessModule module in process.Modules)
+                {
+                    processInfo += $"Module Name: {module.ModuleName}\tFile Name: {module.FileName}\n";
+                }
+
+                MessageBox.Show(processInfo, "Process Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+
+        private void StopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (processList.SelectedItems.Count > 0)
+            {
+                string processName = processList.SelectedItems[0].Text;
+                Process process = Process.GetProcessesByName(processName)[0];
+                try
+                {
+                    process.Kill();
+                    MessageBox.Show($"Процес '{process.ProcessName}' зупинений.",
+                                    "Process Stopped", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Помилка в зупиненні процесу.\nПомилка: {ex.Message}",
+                                    "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
